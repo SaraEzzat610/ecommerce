ff582040df88581b20b99cbeda0ab69c7c95978f
saraezzat610 <sara_liciouz@hotmail.com>
2025-09-09 15:23:09 +0300

    build

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  if (localStorage.getItem("userToken")) {
-    return children;
-  } else {
-    return <Navigate to="/login" />;
-  }
-}
